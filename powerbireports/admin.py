3d1b7b267fb2ba25be40c197232411042abf696c
Ibrahim Khan <ibrahim7353khan@gmail.com>
2022-06-27 16:40:12 +0530

    Export Option

--- powerbireports/admin.py
+++ powerbireports/admin.py
@@ -6,15 +6,27 @@ from .models import *
 
 
 # Register your models here.
-class Search(admin.ModelAdmin):
+
+
+class profileSearchResource(resources.ModelResource):
+    class Meta:
+        model = Profile
+
+class Search(ImportExportModelAdmin):
     search_fields = ("campaignid", "campaign", 'user')
     list_display = ('campaignid', "campaign", 'campaign_type', 'user', 'campaign_status')
     list_filter = ['campaign_type']
+    resource_class = profileSearchResource
 
 
-class MappingSearch(admin.ModelAdmin):
+class MappingSearchResource(resources.ModelResource):
+    class Meta:
+        model = Mapping
+
+class MappingSearch(ImportExportModelAdmin):
     search_fields = ("campaign", "user")
     list_display = ('user', "campaign")
+    resource_class = MappingSearchResource
 
 
 class EmployeesSearchResource(resources.ModelResource):
@@ -30,7 +42,18 @@ class EmployeesSearch(ImportExportModelAdmin):
     list_filter = ['emp_desi']
     resource_class = EmployeesSearchResource
 
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+        fields = ('username', 'id')
 
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+    list_display = ("id", "username",)
+    pass
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Profile, Search)
 admin.site.register(Mapping, MappingSearch)
 admin.site.register(Employees, EmployeesSearch)
